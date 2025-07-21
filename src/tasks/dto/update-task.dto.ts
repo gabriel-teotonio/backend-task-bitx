898214401866420edb6537ab32c09f5730238bcd
gabriel silva <breducar7@gmail.com>
2025-07-21 12:18:21 -0300

    refactor: melhora tratamento de erros em service de comments

--- src/tasks/dto/update-task.dto.ts
+++ src/tasks/dto/update-task.dto.ts
@@ -1,7 +1,14 @@
+import { IsOptional, IsString } from "class-validator";
 import { Status } from "generated/prisma";
 
 export class UpdateTaskDto {
+  @IsString()
+  @IsOptional()
   title?: string;
+  @IsString()
+  @IsOptional()
   description?: string;
+
+  @IsOptional()
   status?: Status;
 } 
